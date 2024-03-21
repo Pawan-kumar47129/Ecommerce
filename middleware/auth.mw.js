1a39ccf920c91b0166253ad6206acf01add6c041
Pawan-kumar47129 <pawan937281@gmail.com>
2024-03-21 17:15:31 +0530

    add validation middleware signup api

---
+++ middleware/auth.mw.js
@@ -0,0 +1,40 @@
+/**
+ * create a mw will check if the request body is proper and correct
+ */
+const user_model=require("../models/user.models")
+const verifySingUpBody=async (req,res,next)=>{
+try{
+//check for the name
+if(!req.body.name){
+    return res.status(400).send({
+        message:"Failed ! Name was not provid in request body"
+    });
+}
+
+// check for the email
+if(!req.body.email){
+    return res.status(400).send({
+        message:"Failed ! Email was not provid in request body"
+    });
+}
+//check for the userId
+if(!req.body.userId){
+    return res.status(400).send({
+        message:"Failed ! userId was not provid in request body"
+    });
+}
+const user=await user_model.find({userId:req.body.userId})
+console.log(user);
+if(user.length===1){
+    return res.status(400).send({message:"Failed ! user with same userId is already present"})
+}
+next();
+//check if user with the same userId is already present
+}catch(err){
+    console.log('error while validating the request object',err);
+    res.status(500).send({
+        message:"Error while validating the request body"
+    })
+}
+}
+module.exports={verifySingUpBody}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,5 @@
 const authController=require('../controllers/auth.controllers');
+const authMw=require('../middleware/auth.mw')
 module.exports=(app)=>{
-    app.post('/ecomm/api/v1/auth/signup',authController.signup)
+    app.post('/ecomm/api/v1/auth/signup',[authMw.verifySingUpBody],authController.signup)
 }
